4d274210d3d08ad432ebd0c997c7d249ee05b212
Greg <gregory.mkv@gmail.com>
2019-12-30 16:21:56 -0500

    Pretty-print error on termination

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,4 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
+#[derive(thiserror::Error)]
 pub enum Error {
     #[error("invalid regex {0}")]
     Regex(#[from] regex::Error),
@@ -10,6 +8,13 @@ pub enum Error {
     TempfilePersist(#[from] tempfile::PersistError),
     #[error("file doesn't have parent path: {0}")]
     InvalidPath(std::path::PathBuf),
+}
+
+// pretty-print the error
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
