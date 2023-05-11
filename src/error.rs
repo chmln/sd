27c217a43ed9c459a987eca5707b3f3e839b156c
CosmicHorror <CosmicHorrorDev@pm.me>
2023-05-10 23:53:57 -0600

    Improve error message for failed replacements (#186)

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,8 @@
+use std::{
+    fmt::{self, Write},
+    path::PathBuf,
+};
+
 #[derive(thiserror::Error)]
 pub enum Error {
     #[error("invalid regex {0}")]
@@ -7,7 +12,27 @@ pub enum Error {
     #[error("failed to move file: {0}")]
     TempfilePersist(#[from] tempfile::PersistError),
     #[error("file doesn't have parent path: {0}")]
-    InvalidPath(std::path::PathBuf),
+    InvalidPath(PathBuf),
+    #[error("failed processing files:\n{0}")]
+    FailedProcessing(FailedJobs),
+}
+
+pub struct FailedJobs(Vec<(PathBuf, Error)>);
+
+impl From<Vec<(PathBuf, Error)>> for FailedJobs {
+    fn from(vec: Vec<(PathBuf, Error)>) -> Self {
+        Self(vec)
+    }
+}
+
+impl fmt::Display for FailedJobs {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("\tFailedJobs(\n")?;
+        for (path, err) in &self.0 {
+            f.write_str(&format!("\t{:?}: {}\n", path, err))?;
+        }
+        f.write_char(')')
+    }
 }
 
 // pretty-print the error
