d46c4845a5bdf6d4b7e5062be238fa946a26554a
Cosmic Horror <CosmicHorrorDev@pm.me>
2023-11-02 16:23:18 -0600

    `cargo xtask gen`

--- gen/completions/sd.fish
+++ gen/completions/sd.fish
@@ -1,7 +1,6 @@
 complete -c sd -s n -d 'Limit the number of replacements' -r
 complete -c sd -s f -l flags -d 'Regex flags. May be combined (like `-f mc`).' -r
-complete -c sd -s p -l preview -d 'Output result into stdout and do not modify files'
+complete -c sd -s p -l preview -d 'Display changes in a human reviewable format (the specifics of the format are likely to change in the future)'
 complete -c sd -s F -l fixed-strings -d 'Treat FIND and REPLACE_WITH args as literal strings'
-complete -c sd -s r -d 'Recursively replace files'
 complete -c sd -s h -l help -d 'Print help (see more with \'--help\')'
 complete -c sd -s V -l version -d 'Print version'
